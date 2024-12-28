fba9b35eb5047b51633568adc24dcf218c649e67
Mauro <mauroignaciomarquez@gmail.com>
2024-12-28 12:47:39 -0300

    Agregamos la rama NAVIDAD

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Bienvenidos a nuestro primer Servidor con Express')
+  res.send('Bienvanidos a nuestro primer servidor con Express')
 })
+
+app.get('/navidad', function (req, res) {
+    res.send('Feliz navidad')
+  })
 
 app.listen(3000)
