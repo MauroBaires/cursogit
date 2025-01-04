a102ddd456fa980a7d5f19c645d7918f62486dc9
Mauro <mauroignaciomarquez@gmail.com>
2025-01-04 10:58:19 -0300

    agregamos data

--- server.js
+++ server.js
@@ -5,4 +5,12 @@ app.get('/', function (req, res) {
   res.send('Bienvenidos a nuestro primer Servidor con Express')
 })
 
+app.get('/navidad', function (req, res) {
+  res.send('Feliz Navidad')
+})
+
+app.get('/otranavidad', function (req, res) {
+  res.send('Feliz navidad y prospero año nuevo')
+})
+
 app.listen(3000)
