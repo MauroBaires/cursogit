e978499cec84197196cac6f024bf22e9354745e5
Mauro <mauroignaciomarquez@gmail.com>
2025-01-04 12:23:04 -0300

    agregamos formulario

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const hbs = require('hbs'); 
 const app = express()
+const PORT = 3000
+
+app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
   res.send('Bienvanidos a nuestro primer servidor con Express')
 })
 
 app.get('/navidad', function (req, res) {
-    res.send('Feliz navidad')
+    res.render('index')
   })
 
-app.listen(3000)
+app.get('/formulario', function (req, res) {
+   res.render('formulario')
+ })
+
+app.listen(PORT, ()=> {
+  console.log (`Servidor corriendo en el puerto http://localhost:${PORT}`)
+})
